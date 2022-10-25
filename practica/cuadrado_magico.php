a5a8e339c6c95f398f76402619a898e0d0eeaf99
raulbrocal <raulbrocalberral16@gmail.com>
2022-10-25 20:11:45 +0200

    Modificación carpetas

---
+++ practica/cuadrado_magico.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title>Cuadrado Mágico</title>
+    <link rel="stylesheet" href="estilos_cuadrado_magico.css">
+</head>
+<body>
+    <div>
+        <?php
+
+            function pintarCuadradoMagico(){
+                echo("<table>");
+                echo("<tr>");
+                echo("<td>");
+                echo("</td>");
+                echo("</tr>");
+                echo("</table>");
+            }
+
+            pintarCuadradoMagico();
+        ?>
+    </div>
+</body>
+</html>

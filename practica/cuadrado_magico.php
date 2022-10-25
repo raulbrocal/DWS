0332f039395ff1a3701331c28b3cdc80c8a56e26
raulbrocal <raulbrocalberral16@gmail.com>
2022-10-25 20:18:03 +0200

    Función pintarCuadradoMagico

--- practica/cuadrado_magico.php
+++ practica/cuadrado_magico.php
@@ -1,23 +1,38 @@
 <html>
+
 <head>
     <title>Cuadrado Mágico</title>
     <link rel="stylesheet" href="estilos_cuadrado_magico.css">
 </head>
+
 <body>
     <div>
         <?php
 
-            function pintarCuadradoMagico(){
-                echo("<table>");
-                echo("<tr>");
-                echo("<td>");
-                echo("</td>");
-                echo("</tr>");
-                echo("</table>");
+        $matriz = [
+            $array = [4, 9, 2],
+            $array = [3, 5, 7],
+            $array = [8, 1, 6]
+        ];
+
+        function pintarCuadradoMagico($matriz)
+        {
+            echo ("<table>");
+
+            for ($i = 0; $i < 3; $i++) {
+                echo ("<tr>");
+                for ($j = 0; $j < 3; $j++) {
+                    echo ("<td>" . $matriz[$i][$j] . "</td>");
+                }
+                echo ("</tr>");
             }
 
-            pintarCuadradoMagico();
+            echo ("</table>");
+        }
+
+        pintarCuadradoMagico($matriz);
         ?>
     </div>
 </body>
+
 </html>

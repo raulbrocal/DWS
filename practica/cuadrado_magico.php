330d51eec2de92595165f4002b1216db1e1ace09
raulbrocal <raulbrocalberral16@gmail.com>
2022-10-27 17:06:23 +0200

    Clase Matriz

--- practica/cuadrado_magico.php
+++ practica/cuadrado_magico.php
@@ -9,20 +9,51 @@
     <div>
         <?php
 
-        $matriz = [
-            $array = [4, 9, 2],
-            $array = [3, 5, 7],
-            $array = [8, 1, 6]
-        ];
+        class Matriz
+        {
+            private $matriz;
+            private $columnas;
 
-        function pintarCuadradoMagico($matriz)
+            function __construct($matriz, $columnas)
+            {
+                $this->matriz = $matriz;
+                $this->columnas = $columnas;
+            }
+
+            public function getMatriz()
+            {
+                return $this->matriz;
+            }
+
+            public function getColumnas(){
+                return $this->columnas;
+            }
+
+        };
+
+        $cuadradoMagico = new Matriz(
+            $matriz = [
+                $array = [4, 9, 2],
+                $array = [3, 5, 7],
+                $array = [8, 1, 6]
+            ],
+            $columnas
+        );
+
+        function pintarCuadradoMagico($cuadradoMagico)
+        {
+            pintarMatriz($cuadradoMagico);
+            analizarCuadradoMagico($cuadradoMagico);
+        };
+
+        function pintarMatriz($matriz)
         {
             echo ("<table>");
 
-            for ($i = 0; $i < 3; $i++) {
+            for ($i = 0; $i < count($matriz->getMatriz()); $i++) {
                 echo ("<tr>");
-                for ($j = 0; $j < 3; $j++) {
-                    echo ("<td>" . $matriz[$i][$j] . "</td>");
+                for ($j = 0; $j < count($matriz->getMatriz()); $j++) {
+                    echo ("<td>" . $matriz->getMatriz()[$i][$j] . "</td>");
                 }
                 echo ("</tr>");
             }
@@ -32,6 +63,19 @@
 
         function analizarCuadradoMagico($matriz)
         {
+            sumaPrimeraFila($matriz);
+        };
+
+        function sumaPrimeraFila($matriz){
+
+            $sumaColumna = 0;
+
+            for ($i=0; $i < count($matriz->getMatriz()[0][$i]); $i++) {
+                $sumaColumna = $sumaColumna + $matriz->getMatriz()[0][$i];
+            }
+
+            echo ("Respecto a la suma de la primera fila que es "); 
+
         }
 
         function analizarFilas($matriz)
@@ -115,7 +159,9 @@
         echo (analizarColumnas($matriz));
         echo (analizarDiagonalPrimera($matriz));
         echo (analizarDiagonalSegunda($matriz));
-        pintarCuadradoMagico($matriz);
+        pintarCuadradoMagico($cuadradoMagico);
+
+        // dentro del objeto debe haber una matriz, un boolean
         ?>
     </div>
 </body>

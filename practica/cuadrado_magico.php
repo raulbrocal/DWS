f6e213dc97501cc58aabc6a4549b5fafb918718d
raulbrocal <raulbrocalberral16@gmail.com>
2022-11-02 13:01:05 +0100

    Borrar

--- practica/cuadrado_magico.php
+++ practica/cuadrado_magico.php
@@ -178,67 +178,6 @@
         $objeto->analizarCuadradoMagico();
         $objeto->pintar();
 
-
-        // function pintarCuadradoMagico($objeto)
-        //  {
-        //      pintarMatriz($objeto);
-        //      analizarCuadradoMagico($objeto);
-        //  };
-
-        //  function pintarMatriz($matriz)
-        //  {
-        //      echo ("<table>");
-
-        //      for ($i = 0; $i < count($matriz->getMatriz()); $i++) {
-        //          echo ("<tr>");
-        //          for ($j = 0; $j < count($matriz->getMatriz()); $j++) {
-        //              echo ("<td>" . $matriz->getMatriz()[$i][$j] . "</td>");
-        //          }
-        //          echo ("</tr>");
-        //      }
-
-        //      echo ("</table>");
-        //  }
-
-        //  function analizarCuadradoMagico($matriz)
-        //  {
-        //      sumaPrimeraFila($matriz);
-        //  };
-
-        //  function sumaPrimeraFila($matriz)
-        //  {
-
-        //      $sumaColumna = 0;
-
-        //      for ($i = 0; $i < count($matriz->getMatriz()[0][$i]); $i++) {
-        //          $sumaColumna = $sumaColumna + $matriz->getMatriz()[0][$i];
-        //      }
-
-        //      return "Respecto a la suma de la primera fila que es " . $sumaColumna;
-        //  }
-
-        //  function analizarFilas($matriz)
-        //  {
-        //      $suma = 0;
-
-        //      for ($i = 0; $i < count($matriz); $i++) {
-
-        //          for ($j = 0; $j < count($matriz); $j++) {
-        //              $suma = $suma + $matriz[$i][$j];
-        //          }
-
-        //          if ($suma != 15) {
-        //              return "false";
-        //          }
-
-        //          $suma = 0;
-        //      }
-
-        //      return "true";
-        //  }
-
-
-
         ?>
     </div>
 </body>

dd33784d1a84891bc9b1c5a4f3075da3a3c88acd
raulbrocal <raulbrocalberral16@gmail.com>
2022-11-11 19:08:44 +0100

    Posible  solucion _GET

--- UD_3/index.php
+++ UD_3/index.php
@@ -1,44 +1,24 @@
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    if (!empty($_GET)) $style = $_GET;
+    if (empty($_GET)) $style = "style";
+    ?>
     <title>Cartelera</title>
-    <link rel="stylesheet" href="estilos.css/style.css">
+    <link rel="stylesheet" href="css/<?php echo $style ?>.css" type="text/css" media="all">
 </head>
 
 <body>
     <div class="contenedor">
         <div class="primera_caja">
-            <a href="index.php">INICIO</a>
-            <a href="pagina1.html">Primera página</a>
-            <a href="pagina2.html">Segunda página</a>
+            <p>Menú</p>
         </div>
         <div class="segunda_caja">
-
-            <div class="primera_columna">
-                <ul>
-                    <li><b>EJERCICIOS</b></li>
-                    <li><a href="./0_hola_mundo.php">1. Hola mundo</a></li>
-                    <li><a href="./1_hola_mundo_comentarios.php">2. Hola mundo comentarios</a></li>
-                    <li><a href="./2_variables_y_tipos.php">3. Variables y tipos</a></li>
-                    <li><a href="./3_arrays_bucles.php">4. Arrays y bucles</a></li>
-                    <li><a href="./4_constantes.php">5. Constantes</a></li>
-                    <li><a href="./5_variables_super_globales.php">6. Variables super globales</a></li>
-                    <li><a href="./6_validar_get.php">7. Ejercicio clase</a></li>
-                    <li><a href="./7_try_catch.php">8. Ejercicio 2 clase</a></li>
-                    <li><a href="./8_informacion.php">9. Ejercicio 3 clase</a></li>
-                    <li><a href="./Ejercicio1/calculadora_usuario.php">Calculadora</a></li>
-                    <li><a href="./cancion.php">Canción</a></li>
-                    <li><a href="./10_ejerExamen.php">Ejercicio exámen</a></li>
-                </ul>
-            </div>
-            <div class="segunda_columna"></div>
-            <div class="tercera_columna">c</div>
-
+            Estilo de película
         </div>
-        <div class="tercera_caja">ccc</div>
+        <div class="tercera_caja"></div>
     </div>
 </body>
 

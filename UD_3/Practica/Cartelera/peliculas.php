18eefde783ac4961e7597f1dcf830c20829070e5
raulbrocal <raulbrocalberral16@gmail.com>
2022-11-17 17:03:44 +0100

    arreglos

--- UD_3/Practica/Cartelera/peliculas.php
+++ UD_3/Practica/Cartelera/peliculas.php
@@ -18,8 +18,7 @@
     <div class="contenedor">
         <div class="primera_caja">
             <ul>
-                <li>Terror</li>
-                <li>Ciencia Ficción</li>
+                <li><a href="categorias.php">Volver</a></li>
             </ul>
         </div>
         <div class="segunda_caja">
@@ -45,7 +44,88 @@
                 <p class="enlace">Enlace:</p>
             </div>
             <div class="pelicula">
-                <p>Adiós</p>
+                <p class="titulo">Halloween: El final</p>
+                <p class="votos">Votos:</p>
+                <p class="puntos">10</p>
+                <div class="imgs">
+                    <img src="imgs/halloweenElFinal.jpg" alt="halloween">
+                </div>
+                <div class="sinopsis">
+                    <h2>Sinopsis</h2>
+                    <br>
+                    <p>
+                        Cuatro años después de los acontecimientos de Halloween Kills, Laurie vive con su nieta Allyson y está a punto de terminar de escribir sus memorias. Nadie ha vuelto a ver a Michael Myers desde entonces. Laurie, después de permitir que el espectro de Myers controlara su realidad durante décadas, ha decidido por fin dejar atrás el miedo y la rabia para dedicarse a vivir.
+
+                        Pero cuando acusan a Corey Cunningham de matar al niño al que cuidaba, se desencadena una cascada de violencia que obligará a Laurie a enfrentarse de una vez por todas con una maldad que no puede controlar.
+                    </p>
+                </div>
+                <br><br>
+                <p class="duracion">Duración: </p>
+                <p class="tiempo"></p>
+                <p class="enlace">Enlace:</p>
+            </div>
+            <div class="pelicula">
+                <p class="titulo">Halloween: El final</p>
+                <p class="votos">Votos:</p>
+                <p class="puntos">10</p>
+                <div class="imgs">
+                    <img src="imgs/halloweenElFinal.jpg" alt="halloween">
+                </div>
+                <div class="sinopsis">
+                    <h2>Sinopsis</h2>
+                    <br>
+                    <p>
+                        Cuatro años después de los acontecimientos de Halloween Kills, Laurie vive con su nieta Allyson y está a punto de terminar de escribir sus memorias. Nadie ha vuelto a ver a Michael Myers desde entonces. Laurie, después de permitir que el espectro de Myers controlara su realidad durante décadas, ha decidido por fin dejar atrás el miedo y la rabia para dedicarse a vivir.
+
+                        Pero cuando acusan a Corey Cunningham de matar al niño al que cuidaba, se desencadena una cascada de violencia que obligará a Laurie a enfrentarse de una vez por todas con una maldad que no puede controlar.
+                    </p>
+                </div>
+                <br><br>
+                <p class="duracion">Duración: </p>
+                <p class="tiempo"></p>
+                <p class="enlace">Enlace:</p>
+            </div>
+            <div class="pelicula">
+                <p class="titulo">Halloween: El final</p>
+                <p class="votos">Votos:</p>
+                <p class="puntos">10</p>
+                <div class="imgs">
+                    <img src="imgs/halloweenElFinal.jpg" alt="halloween">
+                </div>
+                <div class="sinopsis">
+                    <h2>Sinopsis</h2>
+                    <br>
+                    <p>
+                        Cuatro años después de los acontecimientos de Halloween Kills, Laurie vive con su nieta Allyson y está a punto de terminar de escribir sus memorias. Nadie ha vuelto a ver a Michael Myers desde entonces. Laurie, después de permitir que el espectro de Myers controlara su realidad durante décadas, ha decidido por fin dejar atrás el miedo y la rabia para dedicarse a vivir.
+
+                        Pero cuando acusan a Corey Cunningham de matar al niño al que cuidaba, se desencadena una cascada de violencia que obligará a Laurie a enfrentarse de una vez por todas con una maldad que no puede controlar.
+                    </p>
+                </div>
+                <br><br>
+                <p class="duracion">Duración: </p>
+                <p class="tiempo"></p>
+                <p class="enlace">Enlace:</p>
+            </div>
+            <div class="pelicula">
+                <p class="titulo">Halloween: El final</p>
+                <p class="votos">Votos:</p>
+                <p class="puntos">10</p>
+                <div class="imgs">
+                    <img src="imgs/halloweenElFinal.jpg" alt="halloween">
+                </div>
+                <div class="sinopsis">
+                    <h2>Sinopsis</h2>
+                    <br>
+                    <p>
+                        Cuatro años después de los acontecimientos de Halloween Kills, Laurie vive con su nieta Allyson y está a punto de terminar de escribir sus memorias. Nadie ha vuelto a ver a Michael Myers desde entonces. Laurie, después de permitir que el espectro de Myers controlara su realidad durante décadas, ha decidido por fin dejar atrás el miedo y la rabia para dedicarse a vivir.
+
+                        Pero cuando acusan a Corey Cunningham de matar al niño al que cuidaba, se desencadena una cascada de violencia que obligará a Laurie a enfrentarse de una vez por todas con una maldad que no puede controlar.
+                    </p>
+                </div>
+                <br><br>
+                <p class="duracion">Duración: </p>
+                <p class="tiempo"></p>
+                <p class="enlace">Enlace:</p>
             </div>
         </div>
     </div>

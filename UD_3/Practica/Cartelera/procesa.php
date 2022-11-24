72acb8302bdf1392a332e068b3e3956504f5b7a1
raulbrocal <raulbrocalberral16@gmail.com>
2022-11-24 17:11:24 +0100

    método post para voto.php

---
+++ UD_3/Practica/Cartelera/procesa.php
@@ -0,0 +1,4 @@
+<?php
+    echo "el valor del campo nombre_campo_1 es ".$_POST['nombre_campo_1']."<br>";
+
+    echo "el valor del campo nombre_campo_2 es el id de la pelicula: ".$_POST['nombre_campo_2']."<br>";
---
+++ UD_3/Practica/Cartelera/voto.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Votos</title>
+</head>
+<body>
+    <form action="procesa.php" method="POST">
+        <label for="id_campo_1">Etiqueta para el campo 1</label><br>
+        <input id="id_campo_1" name="nombre_campo_1" type="text"><br>
+        <input id="id_campo_2" name="nombre_campo_2" type="hidden" value="1"><br>
+        <input type="submit" value="Aceptar">
+    </form>
+</body>
+</html>

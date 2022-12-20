b8950a8cc195e2fd0635875c211583749361c58e
raulbrocal <raulbrocalberral16@gmail.com>
2022-12-20 07:15:31 +0100

    eliminar php innecesario

--- UD_3/Practica/Cartelera/procesa.php
+++
@@ -1,4 +0,0 @@
-<?php
-    echo "el valor del campo nombre_campo_1 es ".$_POST['nombre_campo_1']."<br>";
-
-    echo "el valor del campo nombre_campo_2 es el id de la pelicula: ".$_POST['nombre_campo_2']."<br>";

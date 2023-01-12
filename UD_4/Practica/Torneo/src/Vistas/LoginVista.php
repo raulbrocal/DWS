5de9a71d4fc20ddf737e7407899ef4040a6e60b8
raulbrocal <raulbrocalberral16@gmail.com>
2023-01-12 17:16:52 +0100

    implementación archivos como requisitos

---
+++ UD_4/Practica/Torneo/src/Vistas/LoginVista.php
@@ -0,0 +1,44 @@
+<?php
+?>
+
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/loginVista.css">
+    <title>Login</title>
+</head>
+
+<body>
+
+    <div class="contenedor">
+        <br><br><br>
+        <div class="titulo">Crea tu propio torneo</div>
+        <br><br><br>
+        <div class="informacion"><div class="form">
+
+            <form method="post" action="" name="signin-form">
+
+                <div class="form-element">
+                    <label>Usuario:</label>
+                    <input type="text" name="username" required />
+                </div>
+
+                <div class="form-element">
+                    <label>Contraseña:</label>
+                    <input type="password" name="password" required />
+                </div>
+
+                <button type="submit" name="login" value="login">Enviar</button>
+
+            </form>
+
+        </div></div>
+        
+    </div>
+
+</body>
+
+</html>
---
+++ UD_4/Practica/Torneo/src/Vistas/gestionTorneosVista.php
@@ -0,0 +1,29 @@
+<?php
+?>
+
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/gestionTorneo.css">
+    <title>Gestionar Torneos</title>
+</head>
+
+<body>
+
+    <div class="contenedor">
+        <br><br><br>
+        <div class="titulo">Gestiona tu torneo</div>
+        <br><br><br>
+        <a href="">Nuevo partido</a>
+        <p class="numRegistros">Número de registros: 7</p>
+        <table class="registros">
+            
+        </table>
+    </div>
+
+</body>
+
+</html>

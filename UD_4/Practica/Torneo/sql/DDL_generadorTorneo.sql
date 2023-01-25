7887e03555529aefa4e4b7c9969356e392d36c26
raulbrocal <raulbrocalberral16@gmail.com>
2023-01-25 15:27:49 +0100

    Actualización BD

--- UD_4/Practica/Torneo/sql/DDL_generadorTorneo.sql
+++ UD_4/Practica/Torneo/sql/DDL_generadorTorneo.sql
@@ -3,8 +3,7 @@ CREATE DATABASE IF NOT EXISTS torneosTenisMesaDB;
 USE torneosTenisMesaDB;
 
 CREATE TABLE T_Usuario (
-    usuarioId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    usuario VARCHAR(255) NOT NULL,
+    usuario VARCHAR(100) NOT NULL PRIMARY KEY,
     clave VARCHAR(255) NOT NULL,
     perfil ENUM('administrador', 'jugador') NOT NULL
 );
@@ -12,9 +11,9 @@ CREATE TABLE T_Usuario (
 CREATE TABLE T_Jugador (
     jugadorId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombreCompleto VARCHAR(200) DEFAULT NULL,
-    usuario INT,
+    usuario VARCHAR(100),
     CONSTRAINT T_Jugador_ibfk_1 FOREIGN KEY (usuario)
-        REFERENCES T_Usuario (usuarioId)
+        REFERENCES T_Usuario (usuario)
 );
 
 CREATE TABLE T_Partido (
@@ -53,17 +52,43 @@ CREATE TABLE T_Torneo_T_Partido (
 INSERT INTO T_Usuario (usuario, clave, perfil) VALUES ('raul', '1234', 'administrador');
 INSERT INTO T_Usuario (usuario, clave, perfil) VALUES ('javi', '1234', 'jugador');
 INSERT INTO T_Usuario (usuario, clave, perfil) VALUES ('sergio', '1234', 'jugador');
+INSERT INTO T_Usuario (usuario, clave, perfil) VALUES ('okolo', '1234', 'jugador');
+INSERT INTO T_Usuario (usuario, clave, perfil) VALUES ('yeray', '1234', 'jugador');
 
-INSERT INTO T_Jugador (nombreCompleto, usuario) VALUES ('Javi Abandono', '2');
-INSERT INTO T_Jugador (nombreCompleto, usuario) VALUES ('Sergio Serrano', '3');
+INSERT INTO T_Jugador (nombreCompleto, usuario) VALUES ('Javi Abandono', 'javi');
+INSERT INTO T_Jugador (nombreCompleto, usuario) VALUES ('Sergio Serrano', 'sergio');
+INSERT INTO T_Jugador (nombreCompleto, usuario) VALUES ('Daniel Okolo', 'okolo');
+INSERT INTO T_Jugador (nombreCompleto, usuario) VALUES ('Yeray Rus', 'yeray');
 
 INSERT INTO T_Partido (ronda, jugadorA, jugadorB, ganador) VALUES ('cuartos', '1', '2', '2');
+INSERT INTO T_Partido (ronda, jugadorA, jugadorB, ganador) VALUES ('cuartos', '3', '4', '3');
 
-INSERT INTO T_Torneo (nombreTorneo, fecha, estado, numJugadores, campeon) VALUES 
-('Torneo IES Son Ferrer','2023-03-25', 'No finalizado', '8', 'Jugador 1');
+INSERT INTO T_Torneo (nombreTorneo, fecha, numJugadores, campeon) VALUES 
+('Torneo IES Son Ferrer','2023-03-25', '8', 'Jugador 1');
 INSERT INTO T_Torneo (nombreTorneo, estado, numJugadores, campeon) VALUES 
 ('Torneo 2021', 'Finalizado', '8', 'Carlos Sogorb');
 INSERT INTO T_Torneo (nombreTorneo, fecha, estado, numJugadores, campeon) VALUES 
 ('Torneo navidad 2022','2022-12-22', 'Finalizado', '8', 'Carlos Acedo');
 
-INSERT INTO T_Torneo_T_Partido VALUES ('1','1');
+INSERT INTO T_Torneo_T_Partido VALUES (1, 1);
+INSERT INTO T_Torneo_T_Partido VALUES (1, 2);
+
+SELECT 
+    ID, jugadorA, jugadorB, ronda, ganador
+FROM
+    T_Torneo
+        INNER JOIN
+    T_Torneo_T_Partido ON ID = torneo
+        INNER JOIN
+    T_Partido ON partidoId = partido;
+
+SELECT 
+    ID, nombreCompleto, nombreCompleto, ronda, ganador
+FROM
+    T_Torneo
+        INNER JOIN
+    T_Torneo_T_Partido ON ID = torneo
+        INNER JOIN
+    T_Partido ON partidoId = partido
+        INNER JOIN
+    T_Jugador ON jugadorId = jugadorA;

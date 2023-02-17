38710179ca9fcb3e98b0bb857d82ead1b44e5dd9
raulbrocal <raulbrocalberral16@gmail.com>
2023-02-17 09:14:08 +0100

    api c#

---
+++ UD_5/API_dotnet6_calculadora/Model/Operations.cs
@@ -0,0 +1,10 @@
+namespace Model
+{
+    public class Operations
+    {
+        public static int CalcularAreaTriangulo(int base_triangulo, int altura)
+        {
+            return (base_triangulo * altura) / 2;
+        }
+    }
+}
